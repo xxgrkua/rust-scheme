893b474b265142e1c98017b20882c1e61d58761e
xxgrkua <xxgrkua@users.noreply.github.com>
2024-01-23 10:05:12 +0900

    Refactor Expression

--- src/parser.rs
+++ src/parser.rs
@@ -1,29 +1,43 @@
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, fmt::Display, rc::Rc};
 
 use crate::{lexer::Token, number::Number};
 
 #[derive(Debug, Clone)]
-pub enum Expression<'a> {
+pub struct Expression<'a> {
+    content: Link<'a>,
+}
+
+impl<'a> Expression<'a> {
+    pub fn car(&self) -> Self {
+        if let Some(expression) = &self.content {
+            if let ExpressionContent::PairLink(pair) = &*expression.borrow() {
+                return Self {
+                    content: pair.car.clone(),
+                };
+            } else {
+                panic!()
+            }
+        } else {
+            panic!()
+        }
+    }
+}
+
+type Link<'a> = Option<Rc<RefCell<ExpressionContent<'a>>>>;
+
+#[derive(Debug, Clone)]
+enum ExpressionContent<'a> {
     Number(Number),
     Symbol(&'a str),
-    Pair(Link<'a>),
-    Nil,
+    PairLink(Pair<'a>),
 }
 
 #[derive(Debug, Clone)]
-enum Link<'a> {
-    End(Rc<Expression<'a>>),
-    More(Rc<Node<'a>>),
-}
-
-#[derive(Debug, Clone)]
-struct Node<'a> {
-    car: Expression<'a>,
+struct Pair<'a> {
+    car: Link<'a>,
     cdr: Link<'a>,
 }
 
 pub fn parse(expr: Vec<Token>) -> Expression {
-    let expr = Expression::Number(Number::Integer(0));
-
-    expr
+    unimplemented!();
 }
